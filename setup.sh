90c6a188c4af49b9df9e88c1e8be842617facba4
Chien-Ming Huang <chienming.h@gmail.com>
2017-06-27 09:42:49 -0400

    fixed directory issue

--- setup.sh
+++ setup.sh
@@ -3,5 +3,6 @@
 rm src/*.txt
 rm src/*.db
 
-python src/ss_init_db.py
-python src/ss_process_story_ods.py source_ods/SAR5_Stories_AA.ods source_ods/SAR5_Stories_MP.ods source_ods/SAR5_Stories_SK.ods
+cd src
+python ss_init_db.py
+python ss_process_story_ods.py ../source_ods/SAR5_Stories_AA.ods ../source_ods/SAR5_Stories_MP.ods ../source_ods/SAR5_Stories_SK.ods
